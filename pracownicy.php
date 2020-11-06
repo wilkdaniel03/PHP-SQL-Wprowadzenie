a37501334f2428590ffb6300f9d5def705d76c26
wilkdaniel03 <73532509+wilkdaniel03@users.noreply.github.com>
2020-11-06 09:17:11 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -1,3 +1,13 @@
+<html>
+<head>
+  <title>Daniel Wilk 2Ti</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="nav">
+    <
+  </div>
 <?php
 
   echo("Tu są pracownicy");
