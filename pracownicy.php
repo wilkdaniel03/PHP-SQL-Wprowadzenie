be1977206172eba6ec58bc27620373b86fd6ebcc
wilkdaniel03 <73532509+wilkdaniel03@users.noreply.github.com>
2020-11-06 08:57:50 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -3,4 +3,24 @@
   <strong><a href="https://github.com/wilkdaniel03/PHP-SQL-Wprowadzenie">Link do mojego githuba</a></strong>
   echo("Tu są pracownicy");
 
+  function wszystko($nr,$sql){
+      require("connect.php");
+
+      echo("<b><br/><br/>Tabelka nr ".$nr."</b>");
+      echo("<b><br/><br/>".$sql."</b>");
+      $result = $conn->query($sql);
+
+      echo("<br/><br/><table border=1>");
+      echo("<th>ID</th>");
+      echo("<th>Imie</th>");
+      echo("<th>Dzial</th>");
+      echo("<th>Zarobki</th>");
+      while($row = $result->fetch_assoc()){
+          echo("<tr>");
+              echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td");
+          echo("</tr>");
+      }
+      echo("</table>");
+  }
+
 ?>
