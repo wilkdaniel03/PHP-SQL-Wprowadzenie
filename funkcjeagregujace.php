dfb6d8e83be5697d54ee56d3f921b6af765abed2
wilkdaniel03 <73532509+wilkdaniel03@users.noreply.github.com>
2020-11-06 12:46:02 +0100

    Update funkcjeagregujace.php

--- funkcjeagregujace.php
+++ funkcjeagregujace.php
@@ -1,5 +1,25 @@
 <?php
 
-  echo("Tu są funkcje agregujące");
+   function wszystko($nr,$sql){
+      require("connect.php");
+
+      echo("<b><br/><br/>Tabelka nr ".$nr."</b>");
+      echo("<b><br/><br/>".$sql."</b>");
+      $result = $conn->query($sql);
+
+      echo("<br/><br/><table border=1>");
+      echo("<th>ID</th>");
+      echo("<th>Imie</th>");
+      echo("<th>Dzial</th>");
+      echo("<th>Zarobki</th>");
+      while($row = $result->fetch_assoc()){
+          echo("<tr>");
+              echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td");
+          echo("</tr>");
+      }
+      echo("</table>");
+   }
+
+   wszystko(1,"SELECT  * FROM pracownicy WHERE dzial=2");
 
 ?>
