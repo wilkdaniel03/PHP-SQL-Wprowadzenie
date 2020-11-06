f0d1bb08fac45ffff7df8d79e5c2fdae62f01a26
wilkdaniel03 <73532509+wilkdaniel03@users.noreply.github.com>
2020-11-06 09:02:06 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -1,6 +1,6 @@
 <?php
 
-  <strong><a href="https://github.com/wilkdaniel03/PHP-SQL-Wprowadzenie">Link do mojego githuba</a></strong>
+  echo("<strong><a href="https://github.com/wilkdaniel03/PHP-SQL-Wprowadzenie">Link do mojego githuba</a></strong>");
   echo("Tu są pracownicy");
 
 ?>
