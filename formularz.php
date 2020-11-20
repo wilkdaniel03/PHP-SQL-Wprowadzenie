1a50352eb591dddc48b5d7b4449b443bed5ec4a8
wilkdaniel03 <73532509+wilkdaniel03@users.noreply.github.com>
2020-11-20 08:44:08 +0100

    Update formularz.php

--- formularz.php
+++ formularz.php
@@ -15,4 +15,29 @@ echo("<h3>Data urodzenia</h3>");
 echo($_POST["urodziny"]);
 echo("</br>");
 
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+$imie = $_POST['imie'];
+$dzial = $_POST['dzial'];
+$zarobki = $_POST['zarobki'];
+$data_urodzenia = $_POST['urodziny'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO Pracownik (null, $_POST['name'], dzial, zarobki) 
+       VALUES (null, $imie, $dzial, $zarobki, $data_urodzenia);
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
