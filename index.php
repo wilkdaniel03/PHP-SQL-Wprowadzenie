cdb3ee0f0e2898fe86566b418536d6d69680eaa2
wilkdaniel03 <73532509+wilkdaniel03@users.noreply.github.com>
2020-11-20 08:36:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <div class="nav">
         <a class="nav-link" href="pracownicy.php">Pracownicy - wstęp</a>
         <a class="nav-link" href="funkcjeagregujace.php">Funkcje Agregujące</a>
-        <a class="nav-link" href="formularz.html">Funkcje Agregujące</a>
+        <a class="nav-link" href="formularz.html">Formularz</a>
     </div>
 </body>
 </html>
