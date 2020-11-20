a25c0d0965c34c9e780c393faba99bba5d7a6b3c
wilkdaniel03 <73532509+wilkdaniel03@users.noreply.github.com>
2020-11-20 08:26:52 +0100

    Create formularz.php

---
+++ formularz.php
@@ -0,0 +1,20 @@
+<?php
+
+echo("jestes na strona.php");
+echo("</br>");
+echo("<h3>Imie</h3>");
+echo($_POST["imie"]);
+echo("</br>");
+echo("<h3>Nazwisko</h3>");
+echo($_POST["nazwisko"]);
+echo("</br>");
+echo("<h3>Pesel</h3>");
+echo($_POST["pesel"]);
+echo("</br>");
+echo("<h3>Urodziny</h3>");
+echo($_POST["urodziny"]);
+echo("</br>");
+echo("<h3>Adres</h3>");
+echo($_POST["adres"]);
+
+?>
